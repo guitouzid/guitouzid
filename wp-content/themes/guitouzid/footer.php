8d6ee4920eb9b66eaaa720a668969dc2ff7e68aa
guitouzid <guitouzid@wanadoo.fr>
2013-12-18 23:08:32 +0100

    Mise en place des bases

---
+++ wp-content/themes/guitouzid/footer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Guitouzid
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer>
+            
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wp-content/themes/guitouzid/index.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme and one
+ * of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query,
+ * e.g., it puts together the home page when no home.php file exists.
+ *
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="main-content" class="main-content">
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+
+		<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) : the_post();
+
+					/*
+					 * Include the post format-specific template for the content. If you want to
+					 * use this in a child theme, then include a file called called content-___.php
+					 * (where ___ is the post format) and that will be used instead.
+					 */
+					//get_template_part( 'content', get_post_format() );
+
+				endwhile;
+			endif;
+		?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+</div><!-- #main-content -->
+
+<?php
+get_footer();
